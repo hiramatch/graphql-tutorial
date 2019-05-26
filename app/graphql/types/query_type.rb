12007d0f1eb65f92057559194f2400715b48f4f7
hiramatch <hiramatch@gmail.com>
2019-05-26 17:01:34 +0900

    Add link query

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,10 +3,17 @@ module Types
     # queries are just represented as fields
     # `all_links` is automatically camelcased to `allLinks`
     field :all_links, [LinkType], null: false
+    field :link, LinkType, null: false do
+      argument :id, ID, required: true
+    end
 
     # this method is invoked, when `all_link` fields is being resolved
     def all_links
       Link.all
+    end
+
+    def link(id:)
+      Link.find(id)
     end
   end
 end
