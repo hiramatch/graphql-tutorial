bcda1ca2ba2a6762eb0f434aa794f03cce1a54c3
hiramatch <hiramatch@gmail.com>
2019-11-02 22:09:45 +0900

    add test commentggggg

--- app/graphql/mutations/base_mutation.rb
+++ app/graphql/mutations/base_mutation.rb
@@ -1,4 +1,5 @@
 module Mutations
+  # test commentggggg
   # test comment
   # This class is used as a parent for all mutations, and it is the place to have common utilities 
   class BaseMutation < GraphQL::Schema::Mutation
